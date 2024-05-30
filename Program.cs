06a23d4aa8af8ce59ea420960b8690bb8ee5426e
Danielle Aragao <danielle.s.aragao@gmail.com>
2024-05-30 18:50:29 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+// implementado
+
+Console.WriteLine("Nokia: ");
+Smartphone n = new Nokia (numero: "153695", modelo: "Modelo 1", imei: "8888", memoria: 32);
+n.Ligar();
+n.InstalarAplicativo("Facebook");
+
+Console.WriteLine("Iphone: ");
+Smartphone i = new Iphone (numero: "5454", modelo: "Modelo 2", imei: "3333", memoria: 64);
+n.Ligar();
+n.InstalarAplicativo("Instagram");
